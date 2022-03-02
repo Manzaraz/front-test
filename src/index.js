52e576e7716270d8c306a9889b1a2469c0b05e8f
Manzaraz <magnusmanz@gmail.com>
2022-03-01 22:21:34 -0300

    second commit

--- src/index.js
+++ src/index.js
@@ -3,13 +3,13 @@ import { ReactDOM, render } from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 const rootElement = document.getElementById("root");
 render(
-  <BrowserRouter>
+  <HashRouter>
     <App />
-  </BrowserRouter>,
+  </HashRouter>,
   rootElement
 );
 
